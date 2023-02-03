f7aa0fab14fb9f1c5c677a8154d662b977e9d152
e28166 <e28166@FXHC140M26.ocsb.local>
2023-02-03 13:43:04 -0500

    Feb.3, 2023

---
+++ src/Sandbox/HelloWorld2.java
@@ -0,0 +1,19 @@
+package Sandbox;
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+
+		// This is a comment
+		
+		/*
+		 This is also another way to comment
+		 multiple lines.
+		 */
+		
+		System.out.println("Hello World again!");
+		
+
+	}
+
+}
