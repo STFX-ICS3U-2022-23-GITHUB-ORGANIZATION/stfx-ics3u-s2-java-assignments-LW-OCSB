cbf997444cc23c8a50331bfb5c1c67ee5d563950
e28166 <e28166@FXHC155m10.ocsb.local>
2023-02-03 10:31:57 -0500

    Feb.3, 2023 - Lesson

---
+++ src/Lessons/HelloWorld.java
@@ -0,0 +1,25 @@
+package Lessons;
+
+public class HelloWorld {
+
+	
+	public static void main(String[] args) {
+		
+		// This is a comment
+		
+		/*
+		 This is also a comment
+		 This is used for multiple lines of comments
+		 */
+		
+		// We can also comment mutliple lines
+		// the harder way!
+		
+		System.out.println("Hello World!");
+		
+		
+	}
+	
+	
+
+}
