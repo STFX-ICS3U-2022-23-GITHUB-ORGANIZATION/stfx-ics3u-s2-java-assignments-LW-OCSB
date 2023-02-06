6a2492772bee47384a69ecc43b98b7f20bb7daa9
e28166 <e28166@FXHC155m10.ocsb.local>
2023-02-06 10:18:45 -0500

    Feb.6, 2023

---
+++ src/Lessons/HelloWorldCLI.java
@@ -0,0 +1,15 @@
+package Lessons;
+
+public class HelloWorldCLI {
+
+	public static void main(String[] args) {
+
+		
+		// This is where we indent
+		
+		System.out.println("Hello World!");
+		
+		
+	}
+
+}
