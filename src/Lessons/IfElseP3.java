e291468e4ba8bd34e28f9190a1d2fe5161ac73b6
e28166 <e28166@FXHC140M26.ocsb.local>
2023-02-07 14:04:46 -0500

    sfd

---
+++ src/Lessons/IfElseP3.java
@@ -0,0 +1,23 @@
+package Lessons;
+
+import java.util.Scanner;
+
+
+public class IfElseP3 {
+
+	public static void main(String[] args) {
+		
+		// Variable Delcarations
+		Scanner sc = new Scanner(System.in);
+		
+		
+		// Input
+		System.out.println("Enter your grade: ");
+		
+		
+		
+		
+
+	}
+
+}
