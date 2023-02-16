74f383730299a78d4ccb17e21405f5da416cf7ce
e28166 <e28166@FXHC140M33.ocsb.local>
2023-02-16 08:39:41 -0500

    2

--- src/Unit1/IncomeTaxBonus.java
+++ src/Unit1/IncomeTaxBonus.java
@@ -37,4 +37,87 @@ public class IncomeTaxBonus {
 		System.out.println("Provincial Tax: " + provTax);
 		System.out.println("Federal Tax: " + fedTax);
 	}	
+	
+	
+	
+	public static double provincialTax(double s)  {
+	
+		double tax = 0;
+		final double PROV_BRACKET_1 = 0.15;  // $53,359 or less
+		final double PROV_BRACKET_2 = 0.205; // over $53,359 up to $106,717
+		final double PROV_BRACKET_3 = 0.26;  // over $106,717 up to $165,430
+		final double PROV_BRACKET_4 = 0.29;  // over $165,430 up to $235,675
+		final double PROV_BRACKET_5 = 0.33;  // over $235,675
+		
+		if (s > 235675)  {
+		
+			// code
+		}
+		
+		else if ( (s > 165430)  && (s < 235675) )  {
+			
+			// code
+		}
+		
+		else if ( (s > 106717)  && (s < 165430) )  {
+			
+			// code
+		}
+		
+		else if ( (s > 53359)  && (s < 106717) )  {
+			
+			// code
+		}
+		
+		else  {  
+			
+			// code 
+		}
+		
+		return tax;
+	}
+	
+
+	public static double federalTax(double s)  {
+	
+		double tax = 0;
+		final double FED_BRACKET_1 = 0.0505;  // $49,231 or less
+		final double FED_BRACKET_2 = 0.0915;  // over $49,231 up to $98,463
+		final double FED_BRACKET_3 = 0.1116;  // over $98,463 up to $150,000
+		final double FED_BRACKET_4 = 0.1216;  // over $150,000 up to $220,000
+		final double FED_BRACKET_5 = 0.1316;  // over $220,000
+		
+		if (s > 220000)  {
+			
+			// code
+		}
+		
+		
+		
+		// HERE
+		
+		else if ( (s > 165430)  && (s < 235675) )  {
+			
+			// code
+		}
+		
+		else if ( (s > 106717)  && (s < 165430) )  {
+			
+			// code
+		}
+		
+		else if ( (s > 53359)  && (s < 106717) )  {
+			
+			// code
+		}
+		
+		else  {  
+			
+			// code 
+		}
+		
+		
+		return tax;
+	}
+
 }
