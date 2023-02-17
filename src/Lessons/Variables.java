f3011e5d066771e523f7ff6094d69ea294610c3b
e28166 <e28166@FXHC140M26.ocsb.local>
2023-02-17 11:51:58 -0500

    Feb.17, 2023

---
+++ src/Lessons/Variables.java
@@ -0,0 +1,17 @@
+package Lessons;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// Variables and data types
+		
+		int myInteger = 2;
+		double myDouble = 13.4234324;
+		char myCharacter = 'x';
+		String myString = "asfsadf";
+		
+		
+
+	}
+
+}
---
+++ src/Sandbox/Sleep.java
@@ -0,0 +1,10 @@
+package Sandbox;
+
+public class Sleep {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
