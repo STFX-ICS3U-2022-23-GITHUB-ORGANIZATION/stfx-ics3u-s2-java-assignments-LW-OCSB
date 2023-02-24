3ab3f84a2e067ac2a49018da8b2ca89523e71344
e28166 <e28166@FXHC140M26.ocsb.local>
2023-02-24 13:37:34 -0500

    Feb.24

---
+++ src/Lessons/Rounding.java
@@ -0,0 +1,22 @@
+package Lessons;
+
+public class Rounding {
+
+	public static void main(String[] args) {
+		
+		double money = 13.567;
+		
+		// Step 1 - move decimal to the right of position you wish to round to
+		money = money * 100;
+		
+		// Step 2 - round to nearest one
+		money = Math.round(money);
+		
+		// Step 3 - move decimal back to original position
+		money = money / 100;
+		
+		System.out.println(money);
+
+	}
+
+}
