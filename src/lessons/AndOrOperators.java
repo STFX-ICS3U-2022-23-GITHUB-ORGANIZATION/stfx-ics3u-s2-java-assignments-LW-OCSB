267091ada74b1f4f2a5075816bba6b6b7477f221
e28166 <e28166@FXHC140M26.ocsb.local>
2023-03-06 13:42:33 -0500

    x

---
+++ src/lessons/AndOrOperators.java
@@ -0,0 +1,27 @@
+package lessons;
+
+public class AndOrOperators {
+
+	public static void main(String[] args) {
+
+		// &&  - and
+		// || - or 
+
+		
+		// both conditions must be true for whole condition to be true
+		if ( (2 > 17)  &&  (3 > 2) )  {
+			
+			System.out.println("Hello World!");
+			
+		}
+		
+		// one condition must be true for whole condition to be true
+		if ( (2 > 17)  ||  (3 > 2) )  {
+					
+			System.out.println("Hello World Again!");
+		}
+		
+		
+	}
+
+}
